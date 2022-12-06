782eef172a8af25c82eb61fcd8c07b8f2881ce5c
Frommflin <freakyfrommelin@outlook.com>
2022-12-06 09:17:34 +0100

    Creating Hardware class

---
+++ W49_AssetTracking2/Hardware.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W49_AssetTracking2
+{
+    internal class Hardware
+    {
+        public string Type { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Price { get; set; }
+        public DateTime DateOfPurchase { get; set; }
+    }
+}
