a5e0b766289024bf6b849b97c02d9c1d53953326
Frommflin <freakyfrommelin@outlook.com>
2022-12-06 11:15:54 +0100

    Adding menu to Asset tracking application

---
+++ W49_AssetTracking2/Methods.cs
@@ -0,0 +1,39 @@
+﻿namespace W49_AssetTracking2
+{
+    internal class Methods
+    {
+        public static void ShowMenu()
+        {
+            string[] menu = { "Show Assets", "Add new Asset", "Exit AssetTracker" };
+
+            Console.WriteLine("Menu: Type the number for the desired option to move on");
+            Console.WriteLine("**************************");
+            for (int i = 0; i < menu.Length; i++)
+            {
+                Console.Write("(");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.Write(i + 1);
+                Console.ResetColor();
+                Console.WriteLine($") {menu[i]}");
+            }
+            Console.WriteLine("**************************");
+        }
+
+        public static void ShowMessage(string message, string color) 
+        {
+            //Showing line of message/instruction in specified color 
+            if(color == "Blue")
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+            } 
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+
+            Console.WriteLine(message);
+            Console.WriteLine();
+            Console.ResetColor();
+        }
+    }
+}
--- W49_AssetTracking2/Program.cs
+++ W49_AssetTracking2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using W49_AssetTracking2;
+
+
+string input;
+
+while (true)
+{
+    Methods.ShowMenu();
+    Console.Write("Go to option: ");
+    input = Console.ReadLine();
+    input.Trim();
+    Console.WriteLine();
+
+    switch (input)
+    {
+        case "1":
+            //Handle showing assets
+            Methods.ShowMessage("Showing all assets!!", "Blue");
+            break;
+        case "2":
+            //Handle adding assets
+            Methods.ShowMessage("Adding a new asset!!", "Blue");
+            break;
+        case "3":
+            // Exiting application
+            Methods.ShowMessage("Shutting down", "Red");
+            return;
+        default:
+            Methods.ShowMessage("Invalid menuoption.", "Red");
+            break;
+    }
+}
